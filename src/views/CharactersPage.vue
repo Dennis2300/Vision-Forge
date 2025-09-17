2ff87492c2bc4659a3cbc993743fc1515437d166
Densanity <denniszixiang@gmail.com>
2025-09-17 20:26:57 +0200

    Update CharactersPage.vue

--- src/views/CharactersPage.vue
+++ src/views/CharactersPage.vue
@@ -1,7 +1,27 @@
 <template>
   <!-- Character Page Container -->
-  <div class="">
-
+  <div class="character-page-container">
+    <div class="character-page-header-container">
+      <div class="character-page-header-banner">
+        <h1 class="character-page-header">Character Page</h1>
+        <img
+          class="character-page-header-image"
+          src="https://act-upload.hoyoverse.com/event-ugc-hoyowiki/2025/07/09/237301566/2627ae5e68e2632df6eeb87b58b6a4f6_8879732995034865752.png?x-oss-process=image%2Fformat%2Cwebp"
+          alt=""
+        />
+      </div>
+      <div class="divider"></div>
+      <p class="character-page-header-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio est
+        nihil soluta consequatur voluptatem obcaecati voluptas illo explicabo
+        iste saepe id, laboriosam laborum adipisci? Minima et molestias sapiente
+        ipsa repellat.
+      </p>
+    </div>
+    <div class="character-page">
+      <div class="offset">Offset</div>
+      <div class="character-display-container">Content</div>
+    </div>
   </div>
 </template>
 
@@ -22,15 +42,59 @@ const error = ref(null);
 // Data states
 const characters = ref([]);
 
-
-
 // Fetch characters on page load
-onMounted(async () => {
-  await GetAllCharacters();
-  isNewCharacter();
-});
+onMounted(async () => {});
 </script>
 
 <style scoped>
+.character-page-container {
+  background-color: darkgreen;
+  width: 1200px;
+  min-height: 100vh;
+  margin: 0px auto;
+}
 
+.character-page-header-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: darkolivegreen;
+}
+
+.character-page-header-banner {
+  background-color: var(--secondary);
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+}
+
+.character-page-header-image {
+  width: 700px;
+  height: 150px;
+  object-fit: cover;
+  overflow: hidden;
+  border-radius: 15px;
+  opacity: 0.4;
+}
+
+.character-page-header {
+  position: absolute;
+  color: white;
+  font-size: 3rem;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  z-index: 1;
+  font-family: var(--font-bungee);
+}
+
+.character-page-header-description {
+  margin-top: 10px;
+  font-size: 1.2rem;
+  text-align: center;
+  color: white;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.7);
+}
 </style>
