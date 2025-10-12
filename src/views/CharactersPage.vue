a5497d6ef193a24b70b4e2caa4c290a8e45de7da
Densanity <denniszixiang@gmail.com>
2025-10-13 00:38:00 +0200

    Update CharactersPage.vue

--- src/views/CharactersPage.vue
+++ src/views/CharactersPage.vue
@@ -1,3 +1,39 @@
-<template>characters page</template>
+<template>
+  <div class="mt-10">
+    <div v-for="character in characters">
+      <h1>{{ character.name }}</h1>
+      <div v-for="region in character.regions">
+        <p>{{ region.region_id.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "./../supabaseClient.js";
+
+const error = ref(null);
+const characters = ref([]);
+
+async function fetchCharacters() {
+  console.log("fetching characters");
+  try {
+    let query = supabase
+      .from("characters")
+      .select(`*, regions:character_region(region_id(name))`);
+
+    const { data, error: supabaseError } = await query;
+    if (supabaseError) throw supabaseError;
+    characters.value = data;
+    console.log(characters.value);
+  } catch (err) {
+    error.value = err.message || "Failed to load characters";
+    console.error(error.value);
+  }
+}
+
+onMounted(() => {
+  fetchCharacters();
+});
+</script>
