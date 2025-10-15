ad1f20422c8c5788bb1118a44cddeec16655a2e6
Densanity <denniszixiang@gmail.com>
2025-10-15 06:03:59 +0200

    Update CharacterDetailPage.vue

--- src/views/CharacterDetailPage.vue
+++ src/views/CharacterDetailPage.vue
@@ -75,25 +75,39 @@
             <div v-else class="text-center">No VA Announced Yet</div>
           </div>
           <div class="flex flex-row">
+            <!-- Regions -->
             <div class="w-1/2">
               <h2 class="divider tracking-wider">Regions</h2>
+
               <div
+                v-if="character.regions?.length"
                 class="list-view tracking-wide flex flex-col items-center gap-2"
               >
-                <p v-for="region in character.regions">
+                <p
+                  v-for="region in character.regions"
+                  :key="region.region_id.id"
+                >
                   {{ region.region_id.name }}
                 </p>
               </div>
+              <div v-else class="text-center">No Regions Revealed</div>
             </div>
+
+            <!-- Affiliation -->
             <div class="w-1/2">
               <h2 class="divider tracking-wider">Affiliation</h2>
               <div
+                v-if="character.affiliations?.length"
                 class="list-view tracking-wide flex flex-col items-center gap-2"
               >
-                <p v-for="affiliation in character.affiliations">
+                <p
+                  v-for="affiliation in character.affiliations"
+                  :key="affiliation.affiliation_id.id"
+                >
                   {{ affiliation.affiliation_id.name }}
                 </p>
               </div>
+              <div v-else class="text-center">No Affiliations Revealed</div>
             </div>
           </div>
         </div>
