d5c399b1067b5399d50caced81c624f24d69149a
Densanity <denniszixiang@gmail.com>
2025-10-20 20:01:17 +0200

    updated footer with all relevant links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-base-200 flex flex-row justify-around items-center p-8">
+  <footer class="bg-base-200 flex flex-row justify-around items-center px-8 py-12">
     <div>
       <div class="flex flex-col gap-5">
         <nav>
@@ -7,63 +7,89 @@
           <div class="grid grid-flow-col gap-4">
             <a href="/about">About</a>
             <a href="https://www.patreon.com/" target="_blank">Patreon</a>
-            <a href="">Github</a>
+            <a href="https://github.com/Dennis2300">Github</a>
           </div>
         </nav>
       </div>
     </div>
     <!--------->
-    <div>
-      <p class="footer-text">
+    <div class="footer-text flex flex-col w-[800px] gap-5 text-center">
+      <p>
         This website is a fan-made project and is not affiliated with, endorsed
-        by, or sponsored by HoYoverse or miHoYo. <br />
-        Character, images, names, and assets used on this site are the property
-        of HoYoverse.
+        by, or sponsored by
+        <strong>HoYoverse</strong> or <strong>miHoYo</strong>. All characters,
+        images, names, and other assets used on this site are the property of
+        <strong>HoYoverse</strong>.
+      </p>
+      <p>
+        Built with <strong><a href="https://vuejs.org/">Vue.js</a></strong> and
+        <strong><a href="https://supabase.com/">Supabase</a></strong>
       </p>
     </div>
     <!-------->
-    <div class="flex flex-col gap-5">
+    <div class="flex flex-col">
       <nav>
         <h6 class="footer-title">Social</h6>
-        <div class="grid grid-flow-col gap-4">
-          <a>
+        <div class="grid grid-flow-col gap-5">
+          <a href="https://www.x.com" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-twitter-x"
+              viewBox="0 0 16 16"
             >
               <path
-                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-              ></path>
+                d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"
+              />
             </svg>
           </a>
-          <a>
+          <a href="https://www.instagram.com" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-instagram"
+              viewBox="0 0 16 16"
             >
               <path
-                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-              ></path>
+                d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
+              />
             </svg>
           </a>
-          <a>
-            <svg
+          <a
+            href="https://www.hoyolab.com/accountCenter/postList?id=112787605"
+            target="_blank"
+          >
+            <img
+              width="25"
+              height="25"
+              src="https://github-production-user-asset-6210df.s3.amazonaws.com/55838591/397082812-a5b02bca-faa8-4fd4-a4d4-3f34c6b7f454.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20251020%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20251020T173142Z&X-Amz-Expires=300&X-Amz-Signature=1f74027fbc6ba63cb114d6be90d59c5b4efc030f367a87ab7f2b142c5c134357&X-Amz-SignedHeaders=host"
+              alt=""
+            />
+          </a>
+          <a href="http://www.discord.com" target="_blank"
+            ><svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
+              width="25"
+              height="25"
+              fill="white"
+              class="bi bi-discord"
+              viewBox="0 0 16 16"
             >
               <path
-                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
-              ></path>
-            </svg>
+                d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612"
+              /></svg
+          ></a>
+          <a href="https://www.patreon.com" target="_blank">
+            <img
+              width="25"
+              height="25"
+              src="./../assets/images/icons/patreon.png"
+              alt=""
+            />
           </a>
         </div>
       </nav>
