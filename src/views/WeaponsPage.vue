e9609c6c0411670871a31e1841ca9884ef69f986
Densanity <denniszixiang@gmail.com>
2025-10-25 20:08:50 +0200

    Update WeaponsPage.vue

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    <h1>Weapons Archive</h1>
+    <!---->
+    <div v-if="loading">
+      <LoadingSpinner />
+    </div>
+    <!---->
+    <div v-else>
+      <h1>Weapon page</h1>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import { supabase } from "./../supabaseClient.js";
+import LoadingSpinner from "./../components/LoadingSpinner.vue";
+
+const loading = ref(false);
+const error = ref(null);
+
+const weapons = ref([]);
+
+async function fetchAllWeapons() {
+  try {
+    let query = supabase.from("weapons").select("*");
+    const { data, error: fetchError } = await query;
+    if (fetchError) throw fetchError;
+    weapons.value = data;
+    console.log(weapons.value);
+  } catch (err) {
+    error.value = err.message || "Failed to fetch weapons";
+  } finally {
+    loading.value = false;
+  }
+}
 
 onMounted(() => {
-  console.log("Page Mounted");
+  loading.value = true;
+  fetchAllWeapons();
 });
 </script>
 
