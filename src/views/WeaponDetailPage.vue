64bf7330be5df10bfa8d8d64f838293271dd60f2
Densanity <denniszixiang@gmail.com>
2025-10-26 19:45:13 +0100

    Update WeaponDetailPage.vue

--- src/views/WeaponDetailPage.vue
+++ src/views/WeaponDetailPage.vue
@@ -2,9 +2,56 @@
   <div v-if="loading">
     <LoadingSpinner />
   </div>
-  <div v-else>
-    <h1>Weapons Page</h1>
-    <h2>{{ weapon.name }}</h2>
+  <div class="weapon-detail-page my-5 bg-secondary mt-10 rounded-2xl" v-else>
+    <!--Header-->
+    <div class="flex justify-around items-center h-96 w-full">
+      <div class="w-1/2 h-full flex flex-col justify-center items-center">
+        <div
+          :class="{
+            'rarity-5': weapon.rarity === 5,
+            'rarity-4': weapon.rarity === 4,
+          }"
+        >
+          <img
+            class="weapon-image w-48 rounded-xl"
+            :src="weapon.image_url"
+            alt=""
+          />
+        </div>
+        <div class="divider px-48"></div>
+        <h1 class="font-acme">{{ weapon.name }}</h1>
+      </div>
+      <div class="w-1/2 h-full flex flex-col justify-center items-center gap-5">
+        <div class="bg-primary w-3/4 h-3/4 p-8 text-tertiary rounded-xl overflow-scroll">
+          <h1 class="text-center">Details</h1>
+          <div class="divider m-0"></div>
+          <h3>
+            <strong class="text-gray-500">Type:</strong> {{ weapon.type.name }}
+          </h3>
+          <div class="divider m-0"></div>
+          <h3>
+            <strong class="text-gray-500">Bonus Effect:</strong>
+            {{ weapon.bonus_effect_type.name }}
+          </h3>
+          <div class="divider m-0"></div>
+          <h3>
+            <strong class="text-gray-500">Value:</strong>
+            {{ weapon.bonus_effect_value }}%
+          </h3>
+          <div class="divider m-0"></div>
+          <h3>
+            <strong class="text-gray-500">Source:</strong>
+            {{ weapon.source.name }}
+          </h3>
+          <div class="divider m-0"></div>
+          <p class="text-white">{{ weapon.common_describe }}</p>
+        </div>
+      </div>
+    </div>
+    <!--Content-->
+    <div>
+      <h1>Content</h1>
+    </div>
   </div>
 </template>
 
@@ -34,12 +81,13 @@ async function fetchWeaponById(weaponId) {
   try {
     let query = supabase
       .from("weapons")
-      .select("*")
+      .select("*, type(name, image_url), bonus_effect_type(name), source(name)")
       .eq("id", weaponId)
       .single();
     const { data, error: fetchError } = await query;
     if (fetchError) throw fetchError;
     weapon.value = data;
+    console.log(weapon.value);
   } catch (err) {
     error.value = err.message || "Failed to load weapon";
   } finally {
@@ -53,3 +101,21 @@ onMounted(async () => {
   await fetchWeaponById(weaponId);
 });
 </script>
+
+<style scoped>
+.weapon-detail-page {
+  width: 1400px;
+  min-height: 100vh;
+}
+
+.weapon-image .rarity-4 {
+  background: linear-gradient(145deg, #e7944a, #b56a2b);
+  box-shadow: 0px 0px 15px rgba(231, 148, 74, 0.8),
+    0px 0px 30px rgba(231, 148, 74, 0.5);
+}
+.weapon-image .rarity-5 {
+  background: linear-gradient(145deg, #9b72d5, #7149a3);
+  box-shadow: 0px 0px 15px rgba(155, 114, 213, 0.8),
+    0px 0px 30px rgba(155, 114, 213, 0.5);
+}
+</style>
