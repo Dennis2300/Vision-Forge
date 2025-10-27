b95b35ec3f22285387c0fd328e9437bc9aa6b0e2
Densanity <denniszixiang@gmail.com>
2025-10-27 17:42:53 +0100

    Update ArtifactsPage.vue

--- src/views/ArtifactsPage.vue
+++ src/views/ArtifactsPage.vue
@@ -1,37 +1,20 @@
 <template>
-  <!--Loading for when fetching is in progress-->
-  <LoadingSpinner v-if="loading" />
-
-  <!-- Display artifacts -->
-  <div v-if="!loading && !error" class="artifacts-page-container">
-    <h1 class="divider mb-10 artifacts-page-header">Artifacts Archive</h1>
-
-    <!--Display data when loading is done-->
-    <div class="artifacts-display-grid">
-      <div v-for="artifact in artifacts" :key="artifact.id">
-        <router-link
-          :to="`/artifacts/${artifact.id}?name=${encodeURIComponent(
-            artifact.name
-          )}`"
-          class="artifact-card"
-        >
-          <img
-            class="artifact-img"
-            :src="artifact.flower_url"
-            :alt="artifact.name"
-            loading="lazy"
-          />
-          <h2 class="truncate artifact-name">
-            {{ artifact.name }}
-          </h2>
-        </router-link>
+  <!--Loader-->
+  <div v-if="loading">
+    <LoadingSpinner />
+  </div>
+  <!--Content-->
+  <div class="artifacts-page" v-else>
+    <!--Header-->
+    <div class="flex flex-col justify-center items-center mt-10">
+      <h1 class="divider tracking-wider px-32">Artifacts Archive</h1>
+    </div>
+    <!--Content-->
+    <div class="grid grid-cols-3 gap-3">
+      <div v-for="artifact in artifacts">
+        <p>{{ artifact.name }}</p>
       </div>
     </div>
-  </div>
-
-  <!-- Display error message -->
-  <div v-if="error">
-    <ErrorComponent />
   </div>
 </template>
 
@@ -46,58 +29,15 @@ const error = ref(null);
 
 const artifacts = ref([]);
 
-const CACHE_DURATION = 1000 * 60 * 60; // 1 hour
-
-function getCachedData(key) {
-  const cachedData = sessionStorage.getItem(key);
-
-  if (!cachedData) {
-    return null;
-  }
-
-  const { timestamp, data } = JSON.parse(cachedData);
-  const now = new Date().getTime();
-
-  if (now - timestamp < CACHE_DURATION) {
-    return data;
-  } else {
-    sessionStorage.removeItem(key);
-    return null;
-  }
-}
-
-function setCachedData(key, data) {
-  const cache = {
-    timestamp: new Date().getTime(),
-    data,
-  };
-  sessionStorage.setItem(key, JSON.stringify(cache));
-}
-
 async function getAllArtifacts() {
-  const cacheKey = "artifacts";
-
-  // Check if cached data exists
-  const cachedArtifacts = getCachedData(cacheKey);
-  if (cachedArtifacts) {
-    artifacts.value = cachedArtifacts;
-    loading.value = false;
-    return;
-  }
-
-  // Fetch data from Supabase
   try {
-    let { data, error: fetchError } = await supabase
-      .from("artifacts")
-      .select("id, name, flower_url")
-      .order("id", { ascending: true });
-
+    let query = supabase.from("artifacts").select("id, name, flower_img_url");
+    const { data, error: fetchError } = await query;
     if (fetchError) throw fetchError;
-
+    console.log(data[0]);
     artifacts.value = data;
-    setCachedData(cacheKey, data); // Cache the fetched data
   } catch (err) {
-    error.value = err;
+    error.value = err.message || "Failed to load characters";
   } finally {
     loading.value = false;
   }
@@ -109,52 +49,8 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.artifacts-page-container {
-  width: 100vw;
-  padding: 50px;
-}
-
-.artifacts-page-header {
-  font-family: var(--font-archivo);
-  font-weight: lighter;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  cursor: default;
-  font-size: 3.5em;
-}
-
-.artifacts-display-grid {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 20px;
-}
-
-.artifact-card {
-  background-color: var(--secondary);
-  font-family: var(--font-acme);
-  padding: 25px;
-  width: 350px;
-  display: flex;
-  justify-items: center;
-  align-items: center;
-  border-radius: 25px;
-  font-size: 1.25em;
-  text-decoration: none;
-  color: var(--tertiary);
-  transition: all 0.3s ease-in-out, transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-.artifact-card:hover {
-  transform: scale(1.03);
-}
-
-.artifact-name {
-  padding-left: 5px;
-}
-
-.artifact-img {
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
+.artifacts-page {
+  width: 1300px;
+  min-height: 100vh;
 }
 </style>
