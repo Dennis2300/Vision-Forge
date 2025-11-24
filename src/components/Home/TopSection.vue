9d7dea7e012d8bfce8c651da587d6e529e61ac84
Densanity <denniszixiang@gmail.com>
2025-11-24 18:00:05 +0100

    Update TopSection.vue

--- src/components/Home/TopSection.vue
+++ src/components/Home/TopSection.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="flex flex-row gap-10 mt-10">
     <!-- left -->
-    <div class="bg-secondary w-2/3 rounded-2xl">
-      <img
-        class="w-full rounded-t-2xl"
-        src="./../../assets/images/zhongli.webp"
-        alt=""
-        loading="lazy"
-      />
+    <div
+      class="bg-secondary w-2/3 rounded-2xl shadow-lg hover:shadow-2xl transition"
+    >
+      <div
+        class="w-full h-[500px] overflow-hidden flex justify-center items-center rounded-t-2xl"
+      >
+        <img
+          class="w-full h-full"
+          src="./../../assets/images/zhongli.webp"
+          alt=""
+          loading="lazy"
+        />
+      </div>
       <div class="p-4">
         <h1 class="">Welcome to Vision Forge</h1>
-        <p class="">
+        <div class="divider m-0"></div>
+
+        <p class="text-gray-300 leading-relaxed">
           Vision Forge is a simple, fan-made space where you can find character
           builds, materials, and team compositions for Genshin Impact. My goal
           is to cut through the complexity and help you forge your perfect
           character.
         </p>
-        <p class="">
+
+        <p class="text-gray-300 leading-relaxed">
           This isn't a pro-level guide, just a fun project where I explore and
           find what works best for me. Hope you find something useful here! and
           enjoy your time in Teyvat!
         </p>
       </div>
     </div>
+
     <!-- right -->
-    <div class="bg-secondary w-1/3 rounded-2xl">
+    <div
+      class="bg-secondary w-1/3 rounded-2xl shadow-lg hover:shadow-2xl transition"
+    >
       <div
         class="w-full h-[500px] overflow-hidden flex justify-center items-center rounded-t-2xl"
       >
@@ -36,13 +48,17 @@
         />
       </div>
       <div class="p-4">
-        <h1 class="">Character Archive</h1>
-        <p class="">
-          Here you can find a comprehensive list of all characters available in
-          Genshin Impact.
+        <h1 class="">Welcome to Vision Forge</h1>
+        <div class="divider m-0"></div>
+
+        <p class="text-gray-300 leading-relaxed text-center">
+          Here you can find a comprehensive list of all playable characters
+          available in Genshin Impact.
         </p>
-        <div class="">
-          <router-link class="" to="/characters"> See Characters </router-link>
+        <div class="flex justify-center mt-4 mb-2">
+          <router-link class="btn btn-primary no-underline" to="/characters">
+            See Characters
+          </router-link>
         </div>
       </div>
     </div>
