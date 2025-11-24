5a9e9c43a936229ad02cc71ec8a4d1ca28b23db2
Densanity <denniszixiang@gmail.com>
2025-11-24 19:35:47 +0100

    Create SupportPopup.vue

---
+++ src/components/Home/SupportPopup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="absolute top-20 right-16 bg-white border shadow-lg p-4 w-64 rounded-b-xl rounded-tl-xl cursor-default"
+  >
+    <p class="text-sm text-gray-700 mb-2">
+      If you like this website, please consider supporting it on Patreon. Every
+      bit helps pay for server hosting and domain fees.
+    </p>
+
+    <a
+      href="https://patreon.com/YOURUSERNAME"
+      target="_blank"
+      class="text-blue-600 no-underline text-sm font-medium hover:underline"
+    >
+      Go to Patreon
+    </a>
+  </div>
+</template>
