f63a7e2a143173702415f7612a1690b752b35e8e
Densanity <denniszixiang@gmail.com>
2025-12-04 19:23:35 +0100

    Update WeaponsPage.vue

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -23,6 +23,7 @@
           :class="{
             'rarity-5': weapon.rarity === 5,
             'rarity-4': weapon.rarity === 4,
+            'rarity-3': weapon.rarity === 3,
           }"
         >
           <img
@@ -76,7 +77,10 @@ async function fetchAllWeapons() {
       weapons.value = cached;
       return;
     }
-    let query = supabase.from("weapons").select("id, name, rarity, image_url");
+    let query = supabase
+      .from("weapons")
+      .select("id, name, rarity, image_url")
+      .order("name", { ascending: true });
     const { data, error: fetchError } = await query;
     if (fetchError) throw fetchError;
     cache("weapons", data);
@@ -113,14 +117,20 @@ onMounted(() => {
   box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
 }
 
-.weapon-image .rarity-4 {
+/* .rarity-3 {
+  background: linear-gradient(145deg, #567496, #3a77b1);
+  box-shadow: 0px 0px 15px rgba(86, 116, 150, 0.8),
+    0px 0px 30px rgba(86, 116, 150, 0.5);
+}
+.rarity-4 {
   background: linear-gradient(145deg, #e7944a, #b56a2b);
   box-shadow: 0px 0px 15px rgba(231, 148, 74, 0.8),
     0px 0px 30px rgba(231, 148, 74, 0.5);
 }
-.weapon-image .rarity-5 {
+
+.rarity-5 {
   background: linear-gradient(145deg, #9b72d5, #7149a3);
   box-shadow: 0px 0px 15px rgba(155, 114, 213, 0.8),
     0px 0px 30px rgba(155, 114, 213, 0.5);
-}
+} */
 </style>
