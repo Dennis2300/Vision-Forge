2c262946a68e7d2f85a8767c804e144062a7775d
Densanity <denniszixiang@gmail.com>
2025-12-06 18:25:50 +0100

    Replace Home page tests with TopSection tests
    Removed Home.test.js and added TopSection.test.js to focus testing on the TopSection component's navigation buttons instead of the Home page.

---
+++ tests/TopSection.test.js
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import TopSection from "../src/components/TopSection.vue";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+
+describe("TopSection buttons", () => {
+  let wrapper;
+  const mockRouter = { push: vi.fn() };
+
+  beforeEach(() => {
+    wrapper = mount(TopSection, {
+      global: {
+        mocks: { $router: mockRouter },
+      },
+    });
+    mockRouter.push.mockClear();
+  });
+
+  it("navigates to /characters when clicking Characters button", async () => {
+    await wrapper.find('[data-test="btn-characters"]').trigger("click");
+    expect(mockRouter.push).toHaveBeenCalledWith("/characters");
+  });
+});
