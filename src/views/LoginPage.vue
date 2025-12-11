46e3251a9e1d51ff5ace39870261defe583f74d8
Densanity <denniszixiang@gmail.com>
2025-12-11 17:17:41 +0100

    Add user authentication logic to LoginPage
    Bind email and password inputs to component state and implement createUser function using Supabase authentication. Added click handlers for create, login, see user, and logout buttons with placeholder logic for all except user creation.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -4,17 +4,47 @@
     <h1>Login</h1>
     <div class="flex flex-row justify-between items-center gap-4">
       <label for="email">Email:</label>
-      <input type="text" name="email" id="email" />
+      <input type="text" name="email" id="email" v-model="email" />
     </div>
     <div class="flex flex-row justify-between items-center gap-4">
       <label for="password">Password:</label>
-      <input type="password" name="password" id="password" />
+      <input type="password" name="password" id="password" v-model="password" />
     </div>
     <div class="mt-6 flex flex-col gap-2">
-      <button>Create</button>
-      <button>Log in</button>
-      <button>See user</button>
-      <button>Log out</button>
+      <button @click="createUser">Create</button>
+      <button @click="login">Log in</button>
+      <button @click="seeCurrentUser">See user</button>
+      <button @click="logout">Log out</button>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { supabase } from "./../supabaseClient";
+
+let email = ref("");
+let password = ref("");
+
+async function createUser() {
+  console.log(email.value, password.value);
+  const { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+  });
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+}
+function login() {
+  console.log("Login");
+}
+function seeCurrentUser() {
+  console.log("Current User");
+}
+function logout() {
+  console.log("Logged out");
+}
+</script>
