0b95e4a79b4c30c811dfd1e34a343a84da423adc
Densanity <denniszixiang@gmail.com>
2025-12-11 18:54:05 +0100

    Display user email on Admin page
    Added logic to fetch and display the current user's email from the Supabase session on the Admin page. Utilized Vue's composition API and lifecycle hooks to retrieve session data on mount.

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>This is the Admin page</h1>
+  <h1>This is the Admin page</h1>
+  <p>Account:</p>
+  <p>{{ account?.data?.session?.user?.email }}</p>
 </template>
+
+<script setup>
+import { supabase } from "./../supabaseClient";
+import { onMounted, ref } from "vue";
+
+const account = ref();
+
+async function getSession() {
+  account.value = await supabase.auth.getSession();
+}
+
+onMounted(() => {
+  getSession();
+});
+</script>
