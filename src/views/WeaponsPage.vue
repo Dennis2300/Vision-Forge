fa9803c6ebcc39c59a9badb7d1772f883c68ec43
Densanity <denniszixiang@gmail.com>
2026-01-07 18:51:23 +0100

    Update WeaponsPage.vue

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -1,17 +1,8 @@
 <template>
-  <!---->
-  <div v-if="loading">
-    <LoadingSpinner />
-  </div>
-  <!---->
-  <div class="weapon-page-container" v-else>
-    <!-- Header -->
-    <div class="flex flex-col justify-center items-center mt-32 mb-10">
-      <h1 class="divider tracking-wider px-32">Weapon Archive</h1>
-    </div>
-    <!-- Grid -->
-    <div class="grid grid-cols-4 gap-8 p-5">
-      <!-- Weapon Card -->
+  <LoadingSpinner v-if="loading" />
+  <div v-else class="weapon-page">
+    <h1 class="divider tracking-wider">Weapon Archive</h1>
+    <div class="grid grid-cols-4 gap-8">
       <RouterLink
         class="weapon-card flex flex-col justify-center items-center py-12 gap-10 rounded-xl"
         v-for="weapon in weapons"
@@ -33,6 +24,36 @@
       </RouterLink>
     </div>
   </div>
+  <!--
+  <div >
+  </div>
+  <div class="weapon-page-container" v-else>
+    <div class="flex flex-col justify-center items-center">
+      <h1 class="divider tracking-wider px-32">Weapon Archive</h1>
+    </div>
+    <div class="grid grid-cols-4 gap-8 p-5">
+      <RouterLink
+        class="weapon-card flex flex-col justify-center items-center py-12 gap-10 rounded-xl"
+        v-for="weapon in weapons"
+        :key="weapon.id"
+        :to="`/weapons/${weapon.id}?name=${encodeURIComponent(weapon.name)}`"
+        target="_blank"
+      >
+        <img
+          class="rounded-2xl pt-2 w-32"
+          :class="{
+            'rarity-5': weapon.rarity === 5,
+            'rarity-4': weapon.rarity === 4,
+            'rarity-3': weapon.rarity === 3,
+          }"
+          :src="weapon.image_url"
+          alt=""
+        />
+        <h3>{{ weapon.name }}</h3>
+      </RouterLink>
+    </div>
+  </div>
+  -->
 </template>
 
 <script setup>
@@ -95,10 +116,9 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.weapon-page-container {
-  width: 1350px;
+.weapon-page {
+  width: 1200px;
   min-height: 100vh;
-  margin: 0px auto;
 }
 
 .weapon-card {
