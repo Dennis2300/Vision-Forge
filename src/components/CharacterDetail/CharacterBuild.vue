d8592a55ae7aa24791ab95049e350227fdfe032c
Densanity <denniszixiang@gmail.com>
2026-01-16 19:00:46 +0100

    Refactor CharacterBuild layout and styling
    Updated the CharacterBuild.vue component to improve layout structure, class usage, and code readability. Combined repeated code blocks, adjusted conditional rendering, and enhanced styling for better UI consistency.

--- src/components/CharacterDetail/CharacterBuild.vue
+++ src/components/CharacterDetail/CharacterBuild.vue
@@ -1,90 +1,77 @@
 <template>
-  <article v-if="character.builds.length > 0">
-    <h1 class="divider mt-20 px-32 mb-5 tracking-wide">Build</h1>
-    <div class="w-full h-auto">
-      <div class="flex flex-row justify-between mx-24 w-auto min-h-96 gap-8">
-        <!--Left-->
-        <div class="bg-primary w-1/3 rounded-2xl p-6 h-fit">
-          <div
-            v-if="character.builds?.length"
-            v-for="build in character.builds"
-          >
-            <h2 class="divider mt-0 tracking-wide">Main Stats</h2>
-            <div class="flex flex-col gap-8">
-              <!-- Sands -->
-              <div
-                class="flex flex-row justify-between"
-                v-for="slot of ['sands']"
-                :key="slot"
-              >
-                <p class="capitalize">{{ slot }}</p>
-                <p class="text-tertiary tracking-wide">
-                  {{
-                    build.build_stat
-                      .filter((stat) => stat.slot === slot)
-                      .map((stat) => stat.stat_id.name)
-                      .join(" or ")
-                  }}
-                </p>
-              </div>
-              <!-- Goblet -->
-              <div
-                class="flex flex-row justify-between"
-                v-for="slot of ['goblet']"
-                :key="slot"
-              >
-                <p class="capitalize">{{ slot }}</p>
-                <p class="text-tertiary tracking-wide">
-                  {{
-                    build.build_stat
-                      .filter((stat) => stat.slot === slot)
-                      .map((stat) => stat.stat_id.name)
-                      .join(" or ")
-                  }}
-                </p>
-              </div>
-              <!-- Circlet -->
-              <div
-                class="flex flex-row justify-between"
-                v-for="slot of ['circlet']"
-                :key="slot"
-              >
-                <p class="capitalize">{{ slot }}</p>
-                <p class="text-tertiary tracking-wide">
-                  {{
-                    build.build_stat
-                      .filter((stat) => stat.slot === slot)
-                      .map((stat) => stat.stat_id.name)
-                      .join(" or ")
-                  }}
-                </p>
-              </div>
-            </div>
-            <h2 class="divider mb-0 mt-6">Substats</h2>
+  <article v-if="character.builds" class="mx-24">
+    <h1 class="divider mb-6 mt-20 px-32 tracking-wide">Builds</h1>
+    <div class="w-full h-auto flex flex-row justify-center gap-8">
+      <div class="w-1/3 h-auto bg-primary p-6 rounded-xl">
+        <div v-if="character.builds?.length" v-for="build in character.builds">
+          <h2 class="divider">Main Stats</h2>
+          <div class="flex flex-col gap-8 mt-4">
             <div
-              class="flex flex-col mt-5"
-              v-for="stat in build.build_stat.filter(
-                (s) => s.slot === 'substats'
-              )"
-              :key="stat.id"
+              v-for="slot of ['sands']"
+              :key="slot"
+              class="flex flex-row justify-between px-4"
             >
+              <p class="capitalize">{{ slot }}</p>
               <p class="text-tertiary tracking-wide">
-                {{ stat.stat_id.name }}
+                {{
+                  build.build_stat
+                    .filter((stat) => stat.slot === slot)
+                    .map((stat) => stat.stat_id.name)
+                    .join(" or ")
+                }}
+              </p>
+            </div>
+            <div
+              v-for="slot of ['goblet']"
+              :key="slot"
+              class="flex flex-row justify-between px-4"
+            >
+              <p class="capitalize">{{ slot }}</p>
+              <p class="text-tertiary tracking-wide">
+                {{
+                  build.build_stat
+                    .filter((stat) => stat.slot === slot)
+                    .map((stat) => stat.stat_id.name)
+                    .join(" or ")
+                }}
+              </p>
+            </div>
+            <div
+              v-for="slot of ['circlet']"
+              :key="slot"
+              class="flex flex-row justify-between px-4"
+            >
+              <p class="capitalize">{{ slot }}</p>
+              <p class="text-tertiary tracking-wide">
+                {{
+                  build.build_stat
+                    .filter((stat) => stat.slot === slot)
+                    .map((stat) => stat.stat_id.name)
+                    .join(" or ")
+                }}
               </p>
             </div>
           </div>
-          <div v-else class="text-center tracking-wide">No Builds Yet</div>
-        </div>
-        <!--Right-->
-        <div class="bg-primary w-2/3 rounded-2xl p-6">
+          <h2 class="divider mt-6">Substats</h2>
           <div
-            v-if="character.builds?.length"
-            v-for="build in character.builds"
+            class="flex flex-col mt-5"
+            v-for="stat in build.build_stat.filter(
+              (s) => s.slot === 'substats'
+            )"
+            :key="stat.id"
           >
-            <MarkdownRender :content="build.notes" />
+            <p class="text-tertiary tracking-wide">
+              {{ stat.stat_id.name }}
+            </p>
           </div>
-          <div class="text-center tracking-wide" v-else>No Build Yet</div>
         </div>
+        <div v-else class="text-center tracking-wide">No Builds Yet</div>
+      </div>
+      <div class="w-2/3 h-auto bg-primary p-6 rounded-xl">
+        <div v-if="character.builds?.length" v-for="build in character.builds">
+          <MarkdownRender :content="build.notes" />
+        </div>
+        <div class="text-center tracking-wide" v-else>No Build Yet</div>
       </div>
     </div>
   </article>
