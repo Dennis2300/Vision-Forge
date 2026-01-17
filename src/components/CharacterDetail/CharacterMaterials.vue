d95706b4d0ac61e2176231ef5826d09b64f2e343
Densanity <denniszixiang@gmail.com>
2026-01-17 17:14:00 +0100

    Update CharacterMaterials.vue

--- src/components/CharacterDetail/CharacterMaterials.vue
+++ src/components/CharacterDetail/CharacterMaterials.vue
@@ -1,6 +1,6 @@
 <template>
   <article>
-    <h1 class="divider">Character Mats</h1>
+    <h1 class="divider mt-20 px-32 mb-8 tracking-wide">Materials</h1>
   </article>
 </template>
 <script setup>
