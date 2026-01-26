cdc58636dcf4952657a14dc3925602d4bc7005fa
Densanity <denniszixiang@gmail.com>
2026-01-26 20:13:19 +0100

    Update WeaponsPage.vue

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -1,6 +1,6 @@
 <template>
   <LoadingSpinner v-if="loading" />
-  <div v-else class="w-3/4 min-h-screen mt-24">
+  <div v-else class="mx-24 mt-4">
     <div class="h-[150px] w-3/4 relative rounded-2xl mx-auto">
       <h1
         class="absolute inset-0 z-20 flex items-center justify-center text-7xl font-acme tracking-wide outline-4"
@@ -18,14 +18,14 @@
     <div class="divider my-4 px-24"></div>
     <div class="grid grid-cols-4 gap-8">
       <RouterLink
-        class="weapon-card flex flex-col justify-center items-center py-12 gap-10 rounded-xl"
         v-for="weapon in weapons"
         :key="weapon.id"
         :to="`/weapons/${weapon.id}?name=${encodeURIComponent(weapon.name)}`"
         target="_blank"
+        class="bg-primary flex flex-col justify-center items-center pt-12 pb-8 gap-8 no-underline rounded-2xl transform hover:-translate-y-2 hover:shadow-xl transition-all duration-300 ease-out group"
       >
         <img
-          class="rounded-2xl pt-2 w-32 h-32"
+          class="w-32 rounded-xl group-hover:scale-110 transition-transform duration-300"
           :class="{
             'rarity-5': weapon.rarity === 5,
             'rarity-4': weapon.rarity === 4,
@@ -34,40 +34,14 @@
           :src="weapon.image_url"
           alt=""
         />
-        <h3>{{ weapon.name }}</h3>
+        <h3
+          class="text-text group-hover:opacity-80 transition-opacity duration-300"
+        >
+          {{ weapon.name }}
+        </h3>
       </RouterLink>
     </div>
   </div>
-  <!--
-  <div >
-  </div>
-  <div class="weapon-page-container" v-else>
-    <div class="flex flex-col justify-center items-center">
-      <h1 class="divider tracking-wider px-32">Weapon Archive</h1>
-    </div>
-    <div class="grid grid-cols-4 gap-8 p-5">
-      <RouterLink
-        class="weapon-card flex flex-col justify-center items-center py-12 gap-10 rounded-xl"
-        v-for="weapon in weapons"
-        :key="weapon.id"
-        :to="`/weapons/${weapon.id}?name=${encodeURIComponent(weapon.name)}`"
-        target="_blank"
-      >
-        <img
-          class="rounded-2xl pt-2 w-32"
-          :class="{
-            'rarity-5': weapon.rarity === 5,
-            'rarity-4': weapon.rarity === 4,
-            'rarity-3': weapon.rarity === 3,
-          }"
-          :src="weapon.image_url"
-          alt=""
-        />
-        <h3>{{ weapon.name }}</h3>
-      </RouterLink>
-    </div>
-  </div>
-  -->
 </template>
 
 <script setup>
@@ -130,27 +104,6 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.weapon-page {
-  width: 1200px;
-  min-height: 100vh;
-}
-
-.weapon-card {
-  text-decoration: none;
-  color: white;
-  background: var(--secondary);
-  transition:
-    transform 0.3s ease,
-    box-shadow 0.3s ease,
-    background 0.3s ease;
-}
-
-.weapon-card:hover {
-  background: var(--filter-color-hover);
-  transform: translateY(-6px) scale(1.03);
-  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
-}
-
 .rarity-5 {
   background: linear-gradient(145deg, #e7944a, #b56a2b);
   box-shadow:
